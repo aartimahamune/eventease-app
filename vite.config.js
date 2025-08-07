2f0850762cb59a899438d112c4e5df5abfffb0ae
aartimahamune <aartimahamune5@gmail.com>
2025-08-07 09:49:21 +0530

    deployement changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/eventease-app/',
   plugins: [react()],
 })
